de52b27c11280e79de478555878bc18f131192b2
Shravani Billingi <shravani3183@gmail.com>
2023-01-26 14:03:05 -0600

    controller

--- src/main/java/com/edj/codefest/controller/TestDemoController.java
+++ src/main/java/com/edj/codefest/controller/TestDemoController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.edj.codefest.database.AddressRepository;
 import com.edj.codefest.database.Customer;
-import com.edj.codefest.database.CustomerRepository;
 import com.edj.codefest.database.CustomerService;
 import com.edj.codefest.models.Account;
 
@@ -18,7 +16,6 @@ import com.edj.codefest.models.Account;
 public class TestDemoController {
 	
 	
-<<<<<<< HEAD
 	@Autowired
 	private CustomerService service;
 	
@@ -30,10 +27,6 @@ public class TestDemoController {
 		 
 		 System.out.println(cust.getAddressId());
 		 System.out.println(cust.getCustomerId());
-=======
-	@GetMapping("accounts/{acctId}")
-	public HttpEntity<Account> getAccount(@PathVariable Long acctId) {
->>>>>>> branch 'main' of https://github.com/Shravani3183/codefest-testapp.git
 		 
 		 Account acct = new Account();
 		 
